fd5f8b51e267ddf051cb65f3df659e1926d50740
bfmcgo2 <https://bfmcgo2@github.com>
2021-01-21 13:53:21 -0500

    katy perry

--- pages/api/genius/get-song.js
+++ pages/api/genius/get-song.js
@@ -4,8 +4,8 @@ export default async (_, res, a) => {
 	const apiKey = process.env.GENIUS_API_TOKEN;
 	const options = {
 		apiKey: process.env.GENIUS_API_TOKEN,
-		title: 'Naive Melody',
-		artist: 'Talking Heads',
+		title: 'Teenage Dream',
+		artist: 'Katy Perry',
 		optimizeQuery: true
 	};
 
