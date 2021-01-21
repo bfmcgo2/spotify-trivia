9a3fe487a8a2b1f471196f3424cb65dd31bbe46e
bfmcgo2 <https://bfmcgo2@github.com>
2021-01-21 12:34:28 -0500

    created mobile user input state

---
+++ components/MobileLogIn.js
@@ -0,0 +1,60 @@
+import { Row, Col, Card, Spacer, useToasts, Button } from "@geist-ui/react";
+import getConfig from 'next/config'
+const { publicRuntimeConfig } = getConfig()
+
+import styles from '../styles/Join.module.css';
+
+const MobileLogIn = ({ room_code, guest, disable, setGuest }) => {
+	const tileColors = ['#FFD29A', '#7CD1F9', '#223441', '#ED5564', '#77ab59'];
+	console.log(publicRuntimeConfig);
+	return(
+		<div className={styles.Join__login}>
+			<div className={styles.Join__label}>
+				<h2 className={styles.Join__h2}>Room Code:</h2>
+				<span className="Join__code">{room_code}</span>
+			</div>
+			
+			<div className={styles.Join__form}>
+				<input type="text" 
+						maxLength = '12' 
+						placeholder="NAME" 
+						className={styles.Join__input}
+						onChange={(e)=> {
+				          setGuest({...guest, name: e.target.value})
+				        }} />
+				<Spacer y={1} />
+				<Button size={'large'}
+						type= {'success'}
+						style={{
+							fontWeight:900,
+							textTransform: 'uppercase'
+						}}
+						disabled={disable}
+						onClick={()=> setGuest({...guest, entered_room: true})}>Enter Game</Button>
+				
+			</div>
+			<div className={styles.Join__label}>
+				<h2 className={styles.Join__h2}>Choose Color:</h2>
+			</div>
+			<Row justify="space-around" style={{ marginBottom: '18px' }}>
+				{tileColors.map((color, i)=> {
+					return(
+						<Col span="4" key={i}>
+							<Card style={{ 
+								backgroundColor: color,
+								boxShadow: (guest.color === color) ? "0px 0px 1px 5px #E0E0E0" : 'none' }} 
+								onClick={()=> {
+									setGuest({...guest, color})
+								}
+							}>
+							</Card>
+						</Col>
+					)
+				})}
+			</Row>
+		</div>
+	)
+
+}
+
+export default MobileLogIn;
---
+++ pages/join/index.js
@@ -0,0 +1,48 @@
+import { useEffect, useContext, useState } from 'react';
+import { Row, Col, Card, Spacer, useToasts, Button } from "@geist-ui/react";
+
+import MobileLogIn from '../../components/MobileLogIn';
+
+import styles from '../../styles/Join.module.css';
+
+const Join = () => {
+	const [room_code, setRoomCode] = useState('');
+	const [guest, setGuest] = useState({});
+	const [disable, setDisable] = useState(true);
+
+	useEffect(()=>{
+		const code = window.location.search.substr(1).split('&')[0].split("=")[1];
+		setRoomCode(code);
+	}, []);
+
+	useEffect(()=> {
+		if(guest.hasOwnProperty('name') && guest.hasOwnProperty('color')) setDisable(false);
+	}, [guest])
+	
+	return (
+	<div className={styles.Join} style={{
+			backgroundColor: (guest.color ? guest.color : 'white')
+		}}>
+		{
+			(guest.entered_room ? 
+				<input type="text" 
+					maxLength = '12' 
+					placeholder="NAME" 
+					className={styles.Join__input}
+					onChange={(e)=> {
+						console.log('hey')
+					}} /> :
+				<MobileLogIn 
+					room_code={ room_code } 
+					guest={ guest }
+					disable = { disable }
+					setGuest = { setGuest } />)
+		}
+		
+		
+	</div>
+	)
+}
+
+
+export default Join
