9e172ab6d4761342e1e6289c7cadcae89806b1d6
bfmcgo2 <https://bfmcgo2@github.com>
2021-07-07 12:37:57 -0600

    update twitter page and added iframe page

--- pages/twitter.js
+++ pages/twitter.js
@@ -5,16 +5,27 @@ const Twitter = () => {
 		<div>
 			<Head>
 				<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
-				
 				<meta name="twitter:card" content="player" />
-					<meta name="twitter:site" content="@rchoi" />
-					<meta name="twitter:title" content="Sample Player Card" />
-					<meta name="twitter:description" content="This is a sample video. When you implement, make sure all links are secure." />
-					<meta name="twitter:player" content="https://spotify-trivia.vercel.app/iframe" />
-					<meta name="twitter:player:width" content="480" />
-					<meta name="twitter:player:height" content="480" />
-					<meta name="twitter:player:stream" content="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+				<meta name="twitter:title" content="Testing for Fenty" />
+				<meta name="twitter:site" content="@BradleyFrancis_"/>
+				<meta name="twitter:player" content="https://spotify-trivia.vercel.app/twitter" />
+				<meta name="twitter:player:width" content="360" />
+				<meta name="twitter:player:height" content="200" />
+				<meta name="twitter:image" content="https://solutions.brightcove.com/bcls/twittercards/bison.jpg" />
 			</Head>
+			<div style={{position: 'relative', display: 'block', maxWidth: '640px'}}>
+				<div style={{paddingTop: "56.25%"}}>
+					<iframe src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" allowfullscreen="" allow="encrypted-media" frameborder="0" style={{
+						position: "absolute",
+						top: "0px",
+						right:"0px",
+						bottom:"0px",
+						left:"0px",
+						width: "100%",
+						height: "100%"
+					}}></iframe>
+				</div>
+			</div>
 		</div>
 	)
 }
