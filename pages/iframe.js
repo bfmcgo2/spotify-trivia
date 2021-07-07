59085721b164d2d968bd112049f64e1b2943e659
bfmcgo2 <https://bfmcgo2@github.com>
2021-07-07 12:15:33 -0600

    adjusted iframe page

--- pages/iframe.js
+++ pages/iframe.js
@@ -3,7 +3,9 @@ import Head from 'next/head'
 const Iframe = () => {
 	return (
 		<div>
-			<iframe src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" frameborder="0"></iframe>
+			<video width="100%" height="100%" controls>
+			  <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4"/>
+			</video>
 		</div>
 	)
 }
