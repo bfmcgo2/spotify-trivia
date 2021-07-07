3f74403a8ccc16fd0de88b05149f963068a940ae
bfmcgo2 <https://bfmcgo2@github.com>
2021-07-07 12:16:42 -0600

    adjusted iframe page

--- pages/iframe.js
+++ pages/iframe.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const Iframe = () => {
 	return (
 		<div>
-			<video width="100%" height="100%" controls>
+			<video width="100%" height="100%" controls autoplay>
 			  <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4"/>
 			</video>
 		</div>
